753d667be09a4865f1e953c70203db3d37274348
Callum Naden <callumnadendesign@gmail.com>
2023-11-20 20:53:41 +1100

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='scroll-smooth'>
+    <html lang="en" className='scroll-smooth  bg-white'>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from './sections/Footer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero/>
       <AboutUs/>
       <Feature/>
